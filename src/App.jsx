d042e500699e345eb5860d4e0e54566da102547f
dhwanisarokar <dhwanisarokar@gmail.com>
2024-03-28 15:40:50 +0530

    impliment full name logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [fullName, setFullName] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setName((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${name.firstName} ${name.lastName}`);
+  };
   return (
     <>
-      <h1>Full Name</h1>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          value={name.firstName}
+          onChange={handleChange}
+          placeholder="First Name"
+          required
+        />
+        <br />
+        <label htmlFor="lastName">First Name: </label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          value={name.lastName}
+          onChange={handleChange}
+          placeholder="Last Name"
+          required
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {fullName.length ? <p>Full Name: {fullName}</p> : ""}
     </>
   );
 }
